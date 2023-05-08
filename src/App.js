04e544442b1e758fb1dcb4cdeae463d01e0725d9
Walton Mupudzi <waltonmupudzi@gmail.com>
2023-05-08 09:44:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Code to import Budget.js
+import Budget from './components/Budget';
+
+// Add code to import the other components here under
+
+
+import { AppProvider } from './context/AppContext';
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                    <div className='row mt-3'>
+                        {/* Add Budget component here under */}        
+                // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                        {/* Add Remaining component here under */}        
+
+                        {/* Add ExpenseTotal component here under */}        
+                       
+                        {/* Add ExpenseList component here under */}         
+
+                        {/* Add ExpenseItem component here under */}        
+
+                        {/* Add AllocationForm component here under */}        
+
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
